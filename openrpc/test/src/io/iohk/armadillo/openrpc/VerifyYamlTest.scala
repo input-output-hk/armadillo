55c0eff27f361561f58675d43977a51832f17762
Kasper Kondzielski <kasper.kondzielski@iohk.io>
2022-05-14 20:25:02 +0200

    Add test for multiple_endpoints

--- openrpc/test/src/io/iohk/armadillo/openrpc/VerifyYamlTest.scala
+++ openrpc/test/src/io/iohk/armadillo/openrpc/VerifyYamlTest.scala
@@ -1,6 +1,7 @@
 package io.iohk.armadillo.openrpc
 
 import cats.effect.IO
+import io.iohk.armadillo._
 import io.iohk.armadillo.openrpc.Basic.{basic, multiple_params, optionalParam, withInfo}
 import io.iohk.armadillo.openrpc.circe.yaml._
 import io.iohk.armadillo.openrpc.model.{OpenRpcDocument, OpenRpcInfo}
@@ -14,19 +15,11 @@ object VerifyYamlTest extends SimpleIOSuite {
 
   private val PetStoreInfo: OpenRpcInfo = OpenRpcInfo("1.0.0", "Demo Pet Store")
 
-  compare(
-    "basic.yaml",
-    OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(basic))
-  )
-
-  compare(
-    "multiple_params.yaml",
-    OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(multiple_params))
-  )
-
+  compare("basic.yaml", OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(basic)))
+  compare("multiple_params.yaml", OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(multiple_params)))
   compare("with_info.yaml", OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(withInfo)))
-
   compare("optional_param.yaml", OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(optionalParam)))
+  compare("multiple_endpoints.yaml", OpenRpcDocsInterpreter().toOpenRpc(PetStoreInfo, List(basic, basic.copy(methodName = m"hello2"))))
 
   private def compare(file: String, document: OpenRpcDocument) = {
     test(file) {
