3535ab59768e2446f158e39a796dc527864139c2
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2022-09-20 18:42:56 +0200

    [KAIZEN] Handle optional params in Json4s implementation

--- json/json4s/src/io/iohk/armadillo/json/json4s/ArmadilloJson4s.scala
+++ json/json4s/src/io/iohk/armadillo/json/json4s/ArmadilloJson4s.scala
@@ -8,7 +8,7 @@ import sttp.tapir.{DecodeResult, Schema}
 import scala.util.{Failure, Success, Try}
 
 trait ArmadilloJson4s {
-  implicit def jsonRpcCodec[H: Schema: Manifest](implicit formats: Formats, serialization: Serialization): JsonRpcCodec[H] =
+  implicit def jsonRpcCodec[H: Schema](implicit formats: Formats, serialization: Serialization, manifest: Manifest[H]): JsonRpcCodec[H] =
     new JsonRpcCodec[H] {
       override type L = JValue
 
@@ -17,7 +17,12 @@ trait ArmadilloJson4s {
       override def schema: Schema[H] = implicitly[Schema[H]]
 
       override def decode(l: JValue): DecodeResult[H] = {
-        Try(l.extract[H]) match {
+        Try(
+          if (manifest.runtimeClass == classOf[Option[_]])
+            l.extractOpt(implicitly, manifest.typeArguments.head).asInstanceOf[H]
+          else
+            l.extract[H]
+        ) match {
           case Failure(exception) => DecodeResult.Error(l.toString, exception)
           case Success(value)     => DecodeResult.Value(value)
         }
