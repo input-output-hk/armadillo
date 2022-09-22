3e039fbac70e2501703d723f6d4c13f527b7c747
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2022-09-22 14:26:56 +0200

    [KAIZEN] Fix optional handling in json4s

--- json/json4s/src/io/iohk/armadillo/json/json4s/ArmadilloJson4s.scala
+++ json/json4s/src/io/iohk/armadillo/json/json4s/ArmadilloJson4s.scala
@@ -2,7 +2,7 @@ package io.iohk.armadillo.json.json4s
 
 import io.iohk.armadillo._
 import org.json4s.JsonAST.JValue
-import org.json4s.{Extraction, Formats, Serialization}
+import org.json4s.{Extraction, Formats, JNothing, JNull, Serialization}
 import sttp.tapir.{DecodeResult, Schema}
 
 import scala.util.{Failure, Success, Try}
@@ -18,9 +18,12 @@ trait ArmadilloJson4s {
 
       override def decode(l: JValue): DecodeResult[H] = {
         Try(
-          if (manifest.runtimeClass == classOf[Option[_]])
-            l.extractOpt(implicitly, manifest.typeArguments.head).asInstanceOf[H]
-          else
+          if (manifest.runtimeClass == classOf[Option[_]]) {
+            (l match {
+              case JNothing | JNull => None
+              case value            => Some(value.extract(implicitly, manifest.typeArguments.head))
+            }).asInstanceOf[H]
+          } else
             l.extract[H]
         ) match {
           case Failure(exception) => DecodeResult.Error(l.toString, exception)
