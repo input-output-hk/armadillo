7ab8e4bc83a47082fb0d26f212987afcc2695db7
Kasper Kondzielski <kasper.kondzielski@iohk.io>
2022-09-23 12:14:39 +0200

    [KAIZEN] Setup nix shells

---
+++ shell.nix
@@ -0,0 +1,12 @@
+# For compatibility with non-flake-enabled Nix versions
+# ...and nix-env-selector
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
